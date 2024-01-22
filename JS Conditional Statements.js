dc49f42861cf2f8a8f6e5478a5a9159a222f11b9
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-01-22 16:27:12 +0530

    Add files via upload

---
+++ JS Conditional Statements.js
@@ -0,0 +1,42 @@
+// for loop
+var sum=0;
+for(var i=0;i<=5;i++)
+{
+    sum+=i;
+}
+console.log(sum);
+
+console.log("\n");
+//while
+var i=1;
+while(i<=10)
+{
+    console.log(i);
+    i++;
+}
+
+console.log("\n");
+//do while
+var j=1;
+var sum1=0;
+do
+{
+    sum1+=j;
+    console.log(sum1);
+    j++;
+}while(j<=5);
+
+console.log("\n");
+// with objects
+var arr=[10,20,30];
+console.log(arr.length);
+for(var i=0;i<arr.length;i++)
+console.log(arr[i])
+
+console.log("\n");
+// with objects
+var arr=[10,20,true,"Sripathy",96.10,false];
+for(var i=0;i<arr.length;i++)
+console.log(arr[i])
+
+
